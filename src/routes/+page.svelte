ad2b3a73b7a48dbff2fe1837b65a63e634876c3b
Runkai Zhang <rzhang04@proton.me>
2023-06-04 09:30:29 -0400

    Add geolocation.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,76 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	let firstQuery = "";
+	let secondQuery = "";
+	
+	async function lookUp(query) {
+		const encodedQuery = encodeURIComponent(query);
+		const url = `https://forward-reverse-geocoding.p.rapidapi.com/v1/search?q=${encodedQuery}&accept-language=en&polygon_threshold=0.0`;
+		const options = {
+		  method: 'GET',
+		  headers: {
+			'X-RapidAPI-Key': 'b16a113bfcmshbdca909b610cfd9p1c6897jsncb9e734f3767',
+			'X-RapidAPI-Host': 'forward-reverse-geocoding.p.rapidapi.com'
+		  }
+		};
+		
+		try {
+			const response = await fetch(url, options);
+			const json = await response.json();
+			const { lat, lon } = json[0];
+			
+			console.log(lat);
+			console.log(lon);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+</script>
+
+<main class="app">
+	<div class="mx-auto max-w-7xl sm:px-6 lg:px-8 my-6">
+	  	<div class="md:flex md:items-center md:justify-between">
+			<div class="min-w-0 flex-1">
+		  	<h2 class="text-2xl font-bold leading-7 text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">TempDiff</h2>
+			</div>
+			<div class="mt-4 flex md:ml-4 md:mt-0">
+		  	<button type="button" class="inline-flex items-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50">Useless</button>
+		  	<button type="button" class="ml-3 inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">Buttons</button>
+			</div>
+	  	</div>
+	</div>
+
+	<div class="mx-auto max-w-7xl sm:px-6 lg:px-8 mb-6">
+		<div class="bg-white shadow sm:rounded-lg">
+	  	<div class="px-4 py-5 sm:p-6">
+			<h3 class="text-base font-semibold leading-6 text-gray-900">Where are you now?</h3>
+			<div class="mt-2 max-w-xl text-sm text-gray-500">
+		  	<p>Input your first location to compare with.</p>
+			</div>
+			<form class="mt-5 sm:flex sm:items-center">
+		  	<div class="w-full sm:m	ax-w-xs">
+				<input type="search" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="New York City" bind:value={firstQuery}>
+		  	</div>
+		  	<button class="mt-3 inline-flex w-full items-center justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:ml-3 sm:mt-0 sm:w-auto" on:click={() => {lookUp(firstQuery)}}>Lookup</button>
+			</form>
+	  	</div>
+		</div>
+	</div>
+	
+	<div class="mx-auto max-w-7xl sm:px-6 lg:px-8">
+		<div class="bg-white shadow sm:rounded-lg">
+		  <div class="px-4 py-5 sm:p-6">
+			<h3 class="text-base font-semibold leading-6 text-gray-900">Where are you going to?</h3>
+			<div class="mt-2 max-w-xl text-sm text-gray-500">
+			  <p>Input your second location to compare against.</p>
+			</div>
+			<form class="mt-5 sm:flex sm:items-center">
+			  <div class="w-full sm:max-w-xs">
+				<input type="search" class="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6" placeholder="Chicago" bind:value={secondQuery}>
+			  </div>
+			  <button class="mt-3 inline-flex w-full items-center justify-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 sm:ml-3 sm:mt-0 sm:w-auto" on:click={() => {lookUp(secondQuery)}}>Lookup</button>
+			</form>
+		  </div>
+		</div>
+	</div>
+</main>
+
